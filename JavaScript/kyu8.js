22fcd8a1a8e560b5af7161924ef717c832cf42b6
Aloxan12 <kvakvaaak@mail.ru>
2022-03-16 13:26:13 +0300

    DNAtoRNA

--- JavaScript/kyu8.js
+++ JavaScript/kyu8.js
@@ -10,3 +10,11 @@ function even_or_odd(number) {
     }
 }
 
+function DNAtoRNA(dna) {
+    return dna.split('')
+        .map((item) => (item.toUpperCase() === 'T' ? 'U' : item))
+        .join('')
+        .replace(/[\s.,%]/g, '')
+    // create a function which returns an RNA sequence from the given DNA sequence
+}
+
