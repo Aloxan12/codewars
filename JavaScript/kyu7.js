de4fbd2edad6121a6bff177e8db232995e493d52
Aloxan12 <kvakvaaak@mail.ru>
2022-03-22 11:15:34 +0300

    checkExam

--- JavaScript/kyu7.js
+++ JavaScript/kyu7.js
@@ -2,7 +2,7 @@ String.prototype.toJadenCase = function () {
     let result = this;
 
     if (this.length !== 0) {
-        result = this.split(" ").map(function(word) {
+        result = this.split(" ").map(function (word) {
             return word.substring(0, 1).toUpperCase() + word.substring(1);
         }).join(" ");
     }
@@ -10,15 +10,15 @@ String.prototype.toJadenCase = function () {
     return result;
 };
 
-function squareDigits(num){
+function squareDigits(num) {
     return +num.toString()
         .split('')
         .map((item) => Math.pow(+item, 2))
         .join('')
 }
 
-function roundToNext5(n){
-    return Math.ceil(n/5)*5
+function roundToNext5(n) {
+    return Math.ceil(n / 5) * 5
 }
 
 function rowSumOddNumbers(n) {
@@ -27,11 +27,11 @@ function rowSumOddNumbers(n) {
 
 function filter_list(l) {
     const newArr = []
-    l.map(item => typeof(item) === "number" ? newArr.push(item) : false)
+    l.map(item => typeof (item) === "number" ? newArr.push(item) : false)
     return newArr
 }
 
-function DNAStrand(dna){
+function DNAStrand(dna) {
     return dna.split('')
         .map((item) =>
             item === 'A'
@@ -56,13 +56,27 @@ function getMiddle(str) {
     return arr.join("");
 }
 
-const binaryArrayToNumber = (arr) => parseInt(arr.join(''),2)
+const binaryArrayToNumber = (arr) => parseInt(arr.join(''), 2)
 
-function highAndLow(numbers){
+function highAndLow(numbers) {
     const newArr = numbers.split(' ').map(item => +item)
     return `${Math.max(...newArr)} ${Math.min(...newArr)}`
 }
 
-function smallEnough(a, limit){
+function smallEnough(a, limit) {
     return Math.max(...a) <= limit ? true : false
+}
+
+const checkExam = (array1, array2) => {
+    let result = 0
+    for (let i = 0; i < array1.length; i++) {
+        if (array2[i] !== '') {
+            if (array1[i] === array2[i]) {
+                result = result + 4
+            } else if (array1[i] !== array2[i]) {
+                result = result - 1
+            }
+        }
+    }
+    return result > 0 ? result : 0
 }
