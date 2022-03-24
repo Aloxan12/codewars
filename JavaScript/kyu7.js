3c827512e28fd4d5d1125c7d81dc3e0b3282a6ba
Aloxan12 <kvakvaaak@mail.ru>
2022-03-24 09:25:13 +0300

    sequenceSum

--- JavaScript/kyu7.js
+++ JavaScript/kyu7.js
@@ -136,3 +136,11 @@ function gimme (triplet) {
         )
         .join('')
 }
+
+const sequenceSum = (begin, end, step) => {
+    let result = 0
+    for (let i = begin; i <= end; i += step) {
+        result += i
+    }
+    return result
+}
