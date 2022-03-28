2ccf479ee0b2b4bad930d23a02674b2d8440fa60
Aloxan12 <kvakvaaak@mail.ru>
2022-03-28 11:20:23 +0300

    summation

--- TypeScript/TypeScript.ts
+++ TypeScript/TypeScript.ts
@@ -1,3 +1,11 @@
 export function numberToString(num: number): string {
     return String(num)
 }
+
+export const summation = (num:number)=> {
+    let result: number = 0
+    for(let i = 0; i<=num; i++){
+        result +=i
+    }
+    return result
+}
