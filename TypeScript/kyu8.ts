182f37c90e9b664667a0107ecb91aa6b180262ee
Aloxan12 <kvakvaaak@mail.ru>
2022-03-29 09:45:47 +0300

    stringToNumber

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -13,3 +13,7 @@ export const summation = (num:number)=> {
 export function simpleMultiplication(num: number): number{
     return num % 2 === 0 ? num * 8 : num * 9;
 }
+
+export function stringToNumber(str: string): number {
+    return Number(str)
+}
