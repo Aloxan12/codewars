fd9cee53e3cfbf629cc9566835de0fe31df4dfd0
Aloxan12 <kvakvaaak@mail.ru>
2022-04-06 00:12:13 +0300

    wallpaper

--- JavaScript/kyu7.js
+++ JavaScript/kyu7.js
@@ -180,3 +180,32 @@ function dotCalculator (equation) {
     }
     return `${'.'.repeat(result)}`
 }
+
+function wallpaper(l, w, h) {
+    const numbers = [
+        'zero',
+        'one',
+        'two',
+        'three',
+        'four',
+        'five',
+        'six',
+        'seven',
+        'eight',
+        'nine',
+        'ten',
+        'eleven',
+        'twelve',
+        'thirteen',
+        'fourteen',
+        'fifteen',
+        'sixteen',
+        'seventeen',
+        'eighteen',
+        'nineteen',
+        'twenty',
+    ]
+    return w * h * l === 0
+        ? numbers[0]
+        : numbers[Math.ceil(((l * h * 2 + w * h * 2) * 1.15) / 5.2)]
+}
