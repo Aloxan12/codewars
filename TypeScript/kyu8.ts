92d6ad05b33a92e31fd316fe3dd2bd545ea22d77
Aloxan12 <kvakvaaak@mail.ru>
2022-04-18 09:39:34 +0300

    centuryFromYear

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -85,3 +85,13 @@ export function countSheeps(arrayOfSheep: (boolean | undefined | null)[]) {
 export function isDivisible(n:number, x:number, y:number):boolean {
     return (n / x) % 1 === 0 && (n / y) % 1 === 0
 }
+
+export const centuryFromYear = (year: number): number => {
+    let century = 100
+    let count = 1
+    while (century < year) {
+        count++
+        century += 100
+    }
+    return year > century ? 0 : count
+};
