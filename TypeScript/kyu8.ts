a47c43cc46d61c6d0aaffd3ccc1f910c7e68086b
Aloxan12 <kvakvaaak@mail.ru>
2022-04-18 09:40:15 +0300

    basicOp

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -99,3 +99,15 @@ export const centuryFromYear = (year: number): number => {
 export function litres(time: number): number {
     return Math.floor(time * 0.5);
 }
+
+export function basicOp(operation: string, value1: number, value2: number): number {
+    if(operation === '+'){
+        return value1 + value2
+    }else if(operation === '-'){
+        return value1 - value2
+    }else if(operation === '*'){
+        return value1 * value2
+    }else{
+        return value1 / value2
+    }
+}
