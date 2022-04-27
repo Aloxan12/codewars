63f44d30f66779448b9be41fd5fb276714bbf612
Aloxan12 <kvakvaaak@mail.ru>
2022-04-27 09:26:55 +0300

    firstNonConsecutive

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -317,6 +317,20 @@ export function stringToArray(s: string): string[] {
     return s.split(' ')
 }
 
+export function firstNonConsecutive (arr: number[]) : null | number {
+    for (let i = 0; i <arr.length-1; i++){
+        if(arr[i+1] !== arr[i]+1){
+            return arr[i+1];
+        }
+    }
+    return null;
+}
+
+
+
+
+
+
 
 
 
