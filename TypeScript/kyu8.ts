ac796702139b99b59569c51347979d7510ffa6e2
Aloxan12 <kvakvaaak@mail.ru>
2022-04-29 12:34:39 +0300

    enough

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -358,7 +358,10 @@ export function doubleChar(str: string): string {
     return str.split('').map(i => i + i).join('')
 }
 
-
+export function enough(cap: number, on: number, wait: number): number {
+    const res = cap - on
+    return res - wait > 0 ? 0 : -(res - wait)
+}
 
 
 
