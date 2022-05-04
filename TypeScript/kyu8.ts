e46825170e4fffa23d5a173b2c917709715a57fe
Aloxan12 <kvakvaaak@mail.ru>
2022-05-04 08:37:03 +0300

    findDifference

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -408,6 +408,9 @@ export function checkForFactor(base: number, factor: number) {
     return base % factor === 0
 }
 
-
+export function findDifference(a: [number, number, number], b: [number, number, number]): number{
+    const result = (a.reduce((a,b)=> a*b) - b.reduce((a,b)=> a*b))
+    return result > 0 ? result : -result
+}
 
 
