b7971fa3fca016a3cf59f05724ab3652972c2f2a
Aloxan12 <kvakvaaak@mail.ru>
2022-05-04 08:35:34 +0300

    switchItUp

--- JavaScript/kyu8.js
+++ JavaScript/kyu8.js
@@ -148,3 +148,7 @@ const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
     }
     return result >= distanceToPump
 }
+
+function switchItUp(number){
+    return ["Zero","One","Two","Three","Four","Five","Six","Seven","Eight","Nine"][number]
+}
