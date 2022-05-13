662afe6aec1901e604250bb2f2ec6e692fe53e3e
Aloxan12 <kvakvaaak@mail.ru>
2022-05-13 10:08:11 +0300

    remove

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -502,3 +502,7 @@ export function helloOrWorld(name = ''): string {
         .map((w, i) => i === 0 ? w.toUpperCase() : w.toLowerCase())
         .join('') : 'World'}!`;
 }
+
+export function remove(s: string): string {
+    return s[s.length - 1] === '!' ? s.slice(0, -1) : s
+}
