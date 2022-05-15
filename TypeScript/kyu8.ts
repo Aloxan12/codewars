8a67075bba675cd290ca92c65d81bbd9f1bb66af
Aloxan12 <kvakvaaak@mail.ru>
2022-05-15 13:42:48 +0300

    weatherInfo

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -527,6 +527,14 @@ export class XOR {
     }
 }
 
+export function weatherInfo(temp: number): string {
+    const c = (temp - 32) * (5/9)
+    if (c < 0)
+        return (Math.round(c*1e5)/1e5 + " is freezing temperature")
+    else
+        return (Math.round(c*1e5)/1e5 + " is above freezing temperature")
+}
+
 export function convertToCelsius(temperature: number): number {
     const celsius = Math.round(((temperature - 32) + (5 / 9)))
     return temperature
