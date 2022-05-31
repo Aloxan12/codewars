4809cdf3b6638e5af920d2ffe4040d02d71fa4d4
Aloxan12 <kvakvaaak@mail.ru>
2022-05-31 10:24:08 +0300

    isIsogram

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -140,3 +140,15 @@ export class G964 {
         return years;
     }
 }
+
+export function isIsogram(str: string): boolean{
+    str = str.toLowerCase();
+    for(let i = 0; i < str.length; ++i) {
+        for(let j = i + 1; j < str.length; ++j) {
+            if(str[i] === str[j]) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
