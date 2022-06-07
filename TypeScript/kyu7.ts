e3a20879a5fe946a9e82bbcb69fee6810a90810f
Aloxan12 <kvakvaaak@mail.ru>
2022-06-07 08:50:29 +0300

    solve

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -229,3 +229,19 @@ export const minValue = (values: number[]): number => {
     // @ts-ignore
     return +[...new Set(values)].sort((a, b) => a - b).join('');
 };
+
+export function solve(s: string) {
+    let lowerNum = 0;
+    let upperNum = 0;
+
+    for(let i = 0; i <= s.length-1; i += 1){
+
+        if(s[i] === s[i].toLowerCase()) {
+            lowerNum += 1;
+        } else {
+            upperNum += 1;
+        }
+    }
+
+    return lowerNum >= upperNum ? s.toLowerCase() : s.toUpperCase();
+}
