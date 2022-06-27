23b78f38cb0dec9ea695d5d3527a9f514d4eed93
Aloxan12 <kvakvaaak@mail.ru>
2022-06-27 09:10:14 +0300

    movie

--- JavaScript/kyu7.js
+++ JavaScript/kyu7.js
@@ -463,3 +463,22 @@ function sortReindeer(reindeerNames) {
         a.split(' ')[1].localeCompare(b.split(' ')[1]),
     )
 }
+
+const check = (str) =>{
+    return str.replace(/zero/g, '0')
+        .replace(/one/g, '1')
+        .replace(/two/g, '2')
+        .replace(/three/g, '3')
+        .replace(/four/g, '4')
+        .replace(/five/g, '5')
+        .replace(/six/g, '6')
+        .replace(/seven/g, '7')
+        .replace(/eight/g, '8')
+        .replace(/nine/g, '9')
+}
+
+// function yandexTest(a,b){
+//     const check =
+//     const firstNum = a
+//
+// }
