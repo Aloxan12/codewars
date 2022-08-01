d68ca4f5ae83d54b382c7340b81824b08df26a1d
Aloxan12 <kvakvaaak@mail.ru>
2022-08-01 10:25:56 +0300

    predictAge

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -420,7 +420,6 @@ export function isPowerOfTwo(n: number): boolean {
     return Number.isInteger(Math.log2(n))
 }
 
-export
-function predictAge(age1:number, age2:number, age3:number,age4:number,age5:number,age6:number,age7:number,age8:number): number{
-    return Math.floor(Math.sqrt([age1, age2, age3,age4,age5,age6,age7,age8].map(i => i * i).reduce((a,b)=> a+b,0)) / 2)
+export function predictAge(age1: number, age2: number, age3: number, age4: number, age5: number, age6: number, age7: number, age8: number): number {
+    return Math.floor(Math.sqrt([age1, age2, age3, age4, age5, age6, age7, age8].map(i => i * i).reduce((a, b) => a + b, 0)) / 2)
 };
