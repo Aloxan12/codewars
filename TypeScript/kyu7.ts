49daf3059e158e4b494c49b3068a332b460b4cfa
Aloxan12 <kvakvaaak@mail.ru>
2022-08-05 08:35:41 +0300

    SeriesSum

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -432,3 +432,17 @@ export function sumTriangularNumbers(n: number): number {
     }
     return total
 }
+
+export function SeriesSum(n:number):string{
+    let result = 0;
+    let reverage = 1;
+    for (let i = 0; i < n; i++) {
+        if (i === 0) {
+            result = 1;
+        } else {
+            reverage += 3;
+            result = result + (1 / reverage);
+        }
+    }
+    return result.toFixed(2);
+}
