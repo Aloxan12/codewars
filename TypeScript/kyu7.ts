07500e1cc4ec9b4c62feb625dd83f8429ca8ee6f
Aloxan12 <kvakvaaak@mail.ru>
2022-08-09 13:25:03 +0300

    gps

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -462,13 +462,12 @@ export class G964_8 {
 
     public static gps = (s, x) => {
 
-        if (x.length<=1) {
+        if (x.length <= 1) {
             return 0;
         }
-
         let output = [];
-        for (let i = 0; i < x.length-1; i++) {
-            output.push((x[i+1]-x[i])*3600/s);
+        for (let i = 0; i < x.length - 1; i++) {
+            output.push((x[i + 1] - x[i]) * 3600 / s);
         }
 
         return Math.max(...output);
