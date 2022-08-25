43dd4848036001419706e956237dbf30e725534b
Aloxan12 <kvakvaaak@mail.ru>
2022-08-25 08:49:57 +0300

    calc

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -534,3 +534,7 @@ export function automorphic(n: number): string {
         ? 'Automorphic'
         : 'Not!!'
 }
+
+export const calc = (str: string): number => {
+    return [...str].map(x => x.charCodeAt(0)).join('').split('').filter(x => x === '7').length * 6
+}
