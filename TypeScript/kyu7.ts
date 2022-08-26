851e2339a58587bc78185e3359b7801fe96f552d
Aloxan12 <kvakvaaak@mail.ru>
2022-08-26 14:17:15 +0300

    isNice

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -544,13 +544,11 @@ export class Kata_1 {
         if (arr.length <= 0) {
             return false;
         }
-
         const result = arr.every(item => {
             return arr.some(current => {
                 return item === current + 1 || item === current - 1;
             });
         });
-
         return result;
     }
 }
