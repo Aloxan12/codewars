3193e8f366c703466f89af3dc01e34c67d2936e3
Aloxan12 <kvakvaaak@mail.ru>
2022-08-30 08:17:10 +0300

    findOdd

--- TypeScript/kyu6.ts
+++ TypeScript/kyu6.ts
@@ -7,3 +7,10 @@ export class Challenge {
         return result.reduce((a, b) => a + b, 0)
     }
 }
+
+export const findOdd = (xs: number[]): number => {
+    const result = xs
+        .map((item) => xs.filter((i) => i === item))
+        .find((item) => item.length % 2 !== 0)
+    return result ? result[0] : 0
+}
