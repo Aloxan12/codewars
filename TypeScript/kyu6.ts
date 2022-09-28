577bc9f485b76a76f468c509953e0ffa9fa27bc0
Aloxan12 <kvakvaaak@mail.ru>
2022-09-28 09:50:56 +0300

    bouncingBall

--- TypeScript/kyu6.ts
+++ TypeScript/kyu6.ts
@@ -158,3 +158,11 @@ export function camelCase (str: string): string {
         )
         .join('')
 }
+
+export function bouncingBall(h: number, bounce: number, window: number): number {
+    if (h > 0 && (bounce > 0 && bounce < 1) && window < h) {
+        return h < window ? -1 : 2 + bouncingBall((h * bounce), bounce, window);
+    } else {
+        return -1;
+    }
+}
