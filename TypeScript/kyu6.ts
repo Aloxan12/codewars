45b0cb9d54967f2e1a5a25ca5da22f16fb8d1f18
Aloxan12 <kvakvaaak@mail.ru>
2022-09-29 10:27:56 +0300

    towerBuilder

--- TypeScript/kyu6.ts
+++ TypeScript/kyu6.ts
@@ -166,3 +166,15 @@ export function bouncingBall(h: number, bounce: number, window: number): number 
         return -1;
     }
 }
+
+export const towerBuilder = (nFloors: number): string[] => {
+    const result: string[] = []
+    for (let i = 0; i < nFloors; i++) {
+        result.push(
+            ' '.repeat(nFloors - i - 1) +
+            '*'.repeat(i * 2 + 1) +
+            ' '.repeat(nFloors - i - 1),
+        )
+    }
+    return result
+}
