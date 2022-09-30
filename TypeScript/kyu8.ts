be9b437ee08bf24a2953d5f844581a67f5ed1648
Aloxan12 <kvakvaaak@mail.ru>
2022-09-30 11:55:53 +0300

    sumOfDifferences

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -733,3 +733,13 @@ export function humanYearsCatYearsDogYears(humanYears: number): [number, number,
                 ? [humanYears, 24 + ((humanYears - 2) * 4), 24 + ((humanYears - 2) * 5)]
                 : [0, 0, 0]
 }
+
+export function sumOfDifferences(arr: number[]): number {
+    arr.sort((a,b) => b - a);
+
+    let sum = 0;
+    for (let i = 0; i < arr.length - 1; ++i) {
+        sum += arr[i] - arr[i + 1];
+    }
+    return sum
+}
