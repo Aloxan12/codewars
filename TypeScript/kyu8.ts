d90c2d0dd3289b08f14ad01b7efcffe995f86d64
Aloxan12 <kvakvaaak@mail.ru>
2022-10-17 08:28:01 +0300

    howManyDalmatians

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -762,8 +762,6 @@ export function shark(pontoonDistance: number, sharkDistance: number, youSpeed: 
 }
 
 export const howManyDalmatians = (number: number): string => {
-
     const dogs: string[] = ["Hardly any", "More than a handful!", "Woah that's a lot of dogs!", "101 DALMATIANS!!!"];
-
     return number <= 10 ? dogs[0] : number <= 50 ? dogs[1] : number === 101 ? dogs[3] : dogs[2]
 }
