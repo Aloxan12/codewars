5a620e12253095d7323162786d251ece39f23fa6
Aloxan12 <kvakvaaak@mail.ru>
2022-10-23 11:09:30 +0300

    Warrior

--- TypeScript/kyu7.ts
+++ TypeScript/kyu7.ts
@@ -620,12 +620,10 @@ function binaryArrayToNumber_for_4_length(arr: number[]): number {
 export class Warrior {
     private name: string
     public health: number
-
     constructor(name: string) {
         this.name = name
         this.health = 100
     }
-
     strike(enemy: Warrior, swings: number) {
         enemy.health = Math.max(0, enemy.health - swings * 10)
     }
