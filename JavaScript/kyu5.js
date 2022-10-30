bd8a80c7f82e397c6692eb750fd0bf4919eb2942
Aloxan12 <kvakvaaak@mail.ru>
2022-10-30 14:17:22 +0300

    getResult

--- JavaScript/kyu5.js
+++ JavaScript/kyu5.js
@@ -25,3 +25,21 @@ function incrementString(str) {
 
     return str.replace(/[0-9]/g, '').concat(num);
 }
+
+
+
+ const students = [
+    {id: 1, name: "Bob"},
+    {id: 2, name: "Alex"},
+    {id: 3, name: "Ann"},
+    {id: 4, name: "Charley"},
+]
+
+ const friends = {
+    1: ["Oliver", "Jack", "Oscar",],
+    2: ["Jack", "Lewis", "Thomas",],
+    3: ["William", "Michael", "Lewis",],
+    4: ["Oscar", "James", "William",],
+}
+
+console.log( friends[students[0].id][3])
