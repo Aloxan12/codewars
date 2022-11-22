908c79009936d23eb6209fc52459c2dbdd00da4e
Aloxan12 <kvakvaaak@mail.ru>
2022-11-22 12:59:14 +0300

    validBraces

--- TypeScript/kyu6.ts
+++ TypeScript/kyu6.ts
@@ -218,3 +218,22 @@ export class G964 {
         }
     }
 }
+
+export function validBraces(braces: string): boolean {
+    let tracer = []
+    for(let i=0;i < braces.length; i++){
+        if ( braces[i] === "(" || braces[i] === "{" || braces[i] === "["){
+            tracer.push(braces[i])
+        } else{
+            if(tracer.length === 0) return false
+            let lastValue = tracer[tracer.length-1]
+            if( (braces[i] === ']' && lastValue === '[') || (braces[i] === '}' && lastValue === '{') || (braces[i] === ')' && lastValue === '('))
+            {
+                tracer.pop()
+            } else {
+                break;
+            }
+        }
+    }
+    return tracer.length === 0
+}
