750cd10e18429898879bed4e434e6cbcbc98d111
Aloxan12 <kvakvaaak@mail.ru>
2022-11-22 12:59:27 +0300

    validBraces

--- TypeScript/kyu6.ts
+++ TypeScript/kyu6.ts
@@ -221,14 +221,13 @@ export class G964 {
 
 export function validBraces(braces: string): boolean {
     let tracer = []
-    for(let i=0;i < braces.length; i++){
-        if ( braces[i] === "(" || braces[i] === "{" || braces[i] === "["){
+    for (let i = 0; i < braces.length; i++) {
+        if (braces[i] === "(" || braces[i] === "{" || braces[i] === "[") {
             tracer.push(braces[i])
-        } else{
-            if(tracer.length === 0) return false
-            let lastValue = tracer[tracer.length-1]
-            if( (braces[i] === ']' && lastValue === '[') || (braces[i] === '}' && lastValue === '{') || (braces[i] === ')' && lastValue === '('))
-            {
+        } else {
+            if (tracer.length === 0) return false
+            let lastValue = tracer[tracer.length - 1]
+            if ((braces[i] === ']' && lastValue === '[') || (braces[i] === '}' && lastValue === '{') || (braces[i] === ')' && lastValue === '(')) {
                 tracer.pop()
             } else {
                 break;
