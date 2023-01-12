1d585a576603ca9b41625e9e11ee6fc3cbc791d0
Aloxan12 <kvakvaaak@mail.ru>
2023-01-12 11:55:12 +0300

    countSmileys

--- TypeScript/kyu6.ts
+++ TypeScript/kyu6.ts
@@ -113,7 +113,7 @@ export function tribonacci([a, b, c]: [number, number, number], n: number): numb
     }
 }
 
-function createPhoneNumber(numbers) {
+function createPhoneNumber(numbers: number[]): string {
     return `(${numbers.slice(0, 3).join('')}) ${numbers
         .slice(3, 6)
         .join('')}-${numbers.slice(6).join('')}`
