9406b181515abc32e21dbbdb167995a045f43e2f
Aloxan12 <kvakvaaak@mail.ru>
2023-04-09 00:55:12 +0300

    Learning TypeScript. Basic Types

--- TypeScript/kyu8.ts
+++ TypeScript/kyu8.ts
@@ -814,3 +814,20 @@ export function correctTail(body: string, tail: string): boolean {
 export function capitalizeWord(word: string): string {
     return word[0].toUpperCase() + word.substring(1)
 }
+
+export enum Color {Red = 1, Green = 2, Blue = 4};
+export const var1Boolean: boolean = true;
+export const var2Decimal: number = 13;
+export const var3Hex: number = 0xf00d;
+export const var4Binary: number =  0b111111;
+export const var5Octal: number = 0o744 ;
+export const var6String: string = "Hello, world!"
+export const var7Array: any[] = [1, 'test', {a: 3}, 4, 5];
+export const var8NumericArray: Array<number> = [1,2,3,4,5];
+export const var9Tuple: [string,number] = ['key', 12345];
+export const var10Enum: Color = Color.Blue;
+export const var11ArrayOfAny: Array<any> = [1, 'test', {a: 3}, 4, 5];
+export const var12VoidFunction = (): void => {console.log("Voiddd")};
+export const var13Null: null = null;
+export const var14Undefined: undefined = undefined;
+export const var15NeverFunction = (): never => {throw new Error("it doesnt work")};
