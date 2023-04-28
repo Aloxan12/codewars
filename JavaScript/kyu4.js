db984fc1f6cc310e1cf94520ea7190dcc7195d49
Aloxan12 <kvakvaaak@mail.ru>
2023-04-28 13:51:54 +0300

    sameCase

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -1,9 +1,40 @@
-function solveExpression(exp) {
+function evaluateExpression(expression) {
+    let result = 0;
+    const operators = {'+': (a, b) => a + b, '-': (a, b) => a - b, '*': (a, b) => a * b};
+
+    let operator = null;
+    let operand = null;
+
+    for (let i = 0; i < expression.length; i++) {
+        const c = expression[i];
+
+        if (operators[c]) {
+            if (operator) {
+                operand = operators[operator](operand, parseInt(expression.substring(i + 1)));
+            } else {
+                operand = parseInt(expression.substring(0, i));
+            }
+
+            operator = c;
+        }
+    }
+
+    if (operator) {
+        result = operators[operator](operand, parseInt(expression.substring(expression.lastIndexOf(operator) + 1)));
+    } else {
+        result = parseInt(expression);
+    }
+
+    return result;
+}
+
+function solveExpression (exp) {
     let res = -1
     for(let i = 0; i < 10; i++){
         const value = exp.replace(/\?/g, `${i}`)
         const answer = value.split('=')[1]
-        const primer = eval(value.split('=')[0])
+        const primer = evaluateExpression(value.split('=')[0])
+        console.log(answer,primer.toString())
         if(answer === primer.toString()){
             res = i;
             break;
