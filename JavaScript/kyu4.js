492d3bd406b9f9af1778a317f4bf13731dabbc66
Aloxan12 <kvakvaaak@mail.ru>
2023-04-28 13:40:30 +0300

    sameCase

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -1,32 +1,17 @@
-// function solveExpression(exp) {
-//     let res = -1
-//     for(let i = 0; i < 10; i++){
-//         const value = exp.replace(/\?/g, `${i}`)
-//         const answer = value.split('=')[1]
-//         const primer = eval(value.split('=')[0])
-//         if(answer === primer.toString()){
-//             res = i;
-//             break;
-//         }
-//     }
-//     return res
-// }
-
 function solveExpression(exp) {
     let res = -1
-    for (let i = 0; i < 10; ) {
+    for(let i = 0; i < 10; i++){
         const value = exp.replace(/\?/g, `${i}`)
         const answer = value.split('=')[1]
         const primer = eval(value.split('=')[0])
-        if (answer === primer.toString()) {
-            res = i
-            break
+        if(answer === primer.toString()){
+            res = i;
+            break;
         }
-        i = (primer > answer) ? i + 1 : (primer < answer) ? i + 1 : i;
     }
     return res
 }
 
 // console.log(solveExpression('123*45?=5?088'))
-// console.log(solveExpression('-5?*-1=5?'))
+console.log(solveExpression('-5?*-1=5?'))
 console.log(solveExpression('??+??=??'))
