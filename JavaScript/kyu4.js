d213a0a822a01108da482254af022ef33794ebe2
Aloxan12 <kvakvaaak@mail.ru>
2023-05-03 12:27:10 +0300

    customEval

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -1,25 +1,29 @@
 
 const customEval = (value)=>{
-    const regex = /(?<first>[-\d\?]*)(?<symbol>[*\-\+])(?<second>[-\d\?]*)/;
+    const regex = /(?<first>[-]?[\d\?]*)(?<symbol>[*\-\+])(?<second>[-]?[\d\?]*)/;
     const matches = value.match(regex);
-    const first = matches?.groups.first; // '2'
-    const symbol = matches?.groups.symbol; // '+'
-    const second = matches?.groups.second; // '2'
-    switch (symbol){
-        case '+': return Number(first) + Number(second)
-        case '*': return Number(first) * Number(second)
-        case '/': return Number(first) / Number(second)
-        default : return Number(first) - Number(second)
+    if(matches && matches.groups){
+        const first = matches.groups.first;
+        const symbol = matches.groups.symbol;
+        const second = matches.groups.second;
+        if(first.length !== 1 && first[0] === '0') return null
+        switch (symbol){
+            case '+': return Number(first) + Number(second)
+            case '*': return Number(first) * Number(second)
+            case '/': return Number(first) / Number(second)
+            default : return Number(first) - Number(second)
+        }
     }
+    return -1
 }
 function solveExpression(exp) {
-    const numbers = [0,1,2,3,4,5,6,7,8,9].filter(item => !exp.split('').some(str => Number(str) === item))
+    const numbers = [0,1,2,3,4,5,6,7,8,9,0].filter(item => !exp.split('').some(str => Number(str) === item))
     let res = -1
     for (let i = 0; i < numbers.length; i++) {
         const value = exp.replace(/\?/g, `${numbers[i]}`)
-        const answer = value.split('=')[1]
-        const primer = customEval(value.split('=')[0])
-        if (answer === primer.toString()) {
+        const [primerText, answer] = value.split('=')
+        const primer = customEval(primerText)
+        if ((primer || primer === 0) && (answer === primer.toString())) {
             res = numbers[i];
             break;
         }
@@ -31,4 +35,18 @@ function solveExpression(exp) {
 // console.log(solveExpression('??+??=??'))
 // console.log(solveExpression('1+1=?'))
 // console.log(solveExpression('?*11=??'))
-console.log(solveExpression('-7715?5--484?00=-28?9?5'))
+// console.log(solveExpression('?*123?45=?'))
+// console.log(solveExpression('?24564+997177=1?21741'))
+// console.log(solveExpression('-7715?5--484?00=-28?9?5'))
+
+function exemple(){
+    for (let i = 0; i< 10; i++){
+        console.log('попали')
+        if(i === 2) {
+            console.log('выход')
+            return 2;
+        }
+    }
+}
+
+console.log('exemple', exemple())
