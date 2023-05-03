df5815244d75aff15d9b1974db3e11e26c07b005
Aloxan12 <kvakvaaak@mail.ru>
2023-05-03 11:35:59 +0300

    customEval

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -13,13 +13,14 @@ const customEval = (value)=>{
     }
 }
 function solveExpression(exp) {
+    const numbers = [0,1,2,3,4,5,6,7,8,9].filter(item => !exp.split('').some(str => Number(str) === item))
     let res = -1
-    for (let i = 0; i < 10; i++) {
-        const value = exp.replace(/\?/g, `${i}`)
+    for (let i = 0; i < numbers.length; i++) {
+        const value = exp.replace(/\?/g, `${numbers[i]}`)
         const answer = value.split('=')[1]
         const primer = customEval(value.split('=')[0])
         if (answer === primer.toString()) {
-            res = i;
+            res = numbers[i];
             break;
         }
     }
@@ -29,4 +30,5 @@ function solveExpression(exp) {
 // console.log(solveExpression('-5?*-1=5?'))
 // console.log(solveExpression('??+??=??'))
 // console.log(solveExpression('1+1=?'))
-console.log(solveExpression('?*11=??'))
+// console.log(solveExpression('?*11=??'))
+console.log(solveExpression('-7715?5--484?00=-28?9?5'))
