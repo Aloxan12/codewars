5d1fd5d7281aad0ddda12e6c4df53cdcaae44cab
Aloxan12 <kvakvaaak@mail.ru>
2023-05-11 14:42:02 +0300

    alphabetPosition

--- JavaScript/kyu6.js
+++ JavaScript/kyu6.js
@@ -513,5 +513,3 @@ function toCamelCase(str) {
 function alphabetPosition(text) {
     return text.replace(/[^A-Za-z]/g, '').toLowerCase().split('').map(item => item.charCodeAt(0) - 96).join(' ');
 }
-
-console.log(alphabetPosition('The sunset sets at twelve'))
