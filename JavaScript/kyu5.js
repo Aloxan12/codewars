b2b0fd17da1a23ff01d8a0b3110062a496de8c8f
Aloxan12 <kvakvaaak@mail.ru>
2023-05-15 12:33:49 +0300

    pigIt

--- JavaScript/kyu5.js
+++ JavaScript/kyu5.js
@@ -36,7 +36,10 @@ function moveZeros(arr) {
 }
 
 function pigIt(str){
-    return str
+    return str.split(' ').map(item => {
+        if(/[A-Za-z]/g.test(item)){
+            return item.slice(1) + item[0] + 'ay'
+        }
+        return item
+    }).join(' ')
 }
-
-console.log(pigIt('Pig latin is cool'))
