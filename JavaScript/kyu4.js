221046e339fa9c3fabd21024e93288dd9aa2379e
Aloxan12 <kvakvaaak@mail.ru>
2023-05-15 14:27:32 +0300

    pigIt

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -35,3 +35,52 @@ function solveExpression(exp) {
 // console.log(solveExpression('?*123?45=?'))
 // console.log(solveExpression('?24564+997177=1?21741'))
 // console.log(solveExpression('-7715?5--484?00=-28?9?5'))
+
+
+
+
+// Snail Sort
+
+snail = function(array) {
+    if(array.length < 3){
+        return array.reduce((acc, el)=> [...acc, ...el], [])
+    }
+    if(array.length === 3){
+        const [arr1, arr2, arr3] = array
+        return [...arr1, arr2.reverse()[0], ...arr3.reverse(), ...arr2.reverse().slice(0, -1)]
+    }
+    const result = []
+    array.map((item, index, arr) => {
+        const lastInd = arr.length - 1
+        if(index === 0){
+            result.push(...item)
+        }
+        if(index !== 0 && index !== lastInd){
+            result.push(item.reverse()[0])
+            item.shift()
+        }
+        if(index === lastInd){
+            result.push(...item.reverse())
+        }
+    })
+    array.reverse().map((item, index, arr) => {
+        const lastInd = arr.length - 1
+        if(index === 0)return item
+        if(index !== 0 && index !== lastInd){
+            result.push(item.reverse()[0])
+            item.shift()
+        }
+        if(index === lastInd)return item
+    })
+    array.reverse()
+    const trimmedArray = array.slice(1, -1);
+    return [...result, ...snail(trimmedArray)]
+}
+
+console.log(snail([[1]]))
+console.log(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
+console.log(snail([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]))
+console.log(snail([[1, 2, 3, 4, 5, 6], [20, 21, 22, 23, 24, 7], [19, 32, 33, 34, 25, 8], [18, 31, 36, 35, 26, 9], [17, 30, 29, 28, 27, 10], [16, 15, 14, 13, 12, 11]]))
+
+// [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 36, 35 ]
+// [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 ]
