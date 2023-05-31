c6b8e6ce8eb5eec2716c9af64b176b28ddce4e93
Aloxan12 <kvakvaaak@mail.ru>
2023-05-31 10:06:42 +0300

    formatDuration

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -86,30 +86,3 @@ snail = function(array) {
 // console.log(snail([[1, 2, 3], [4, 5, 6], [7, 8, 9]]))
 // console.log(snail([[1, 2, 3, 4, 5], [6, 7, 8, 9, 10], [11, 12, 13, 14, 15], [16, 17, 18, 19, 20], [21, 22, 23, 24, 25]]))
 // console.log(snail([[1, 2, 3, 4, 5, 6], [20, 21, 22, 23, 24, 7], [19, 32, 33, 34, 25, 8], [18, 31, 36, 35, 26, 9], [17, 30, 29, 28, 27, 10], [16, 15, 14, 13, 12, 11]]))
-
-
-
-// function formatDuration(seconds) {
-//     let minutes = Math.floor(seconds / 60);
-//     const sec = seconds % 60;
-//
-//     let hours = Math.floor(seconds / 3600);
-//     minutes = minutes % 60;
-//
-//     let days = Math.floor(hours / 24);
-//     hours = hours % 24;
-//
-//     let years = Math.floor(days / 365);
-//     days = days % 365;
-//
-//     if(!seconds)return 0
-//     if(!sec && !minutes && !hours && !days && !years)return 0
-//     if(!seconds)return 0
-//     if(!seconds)return 0
-//     return `${years} years, ${days} days, ${hours} hours, ${minutes} minutes, ${sec} seconds`;
-// }
-//
-// console.log(formatDuration(2))
-// console.log(formatDuration(62))
-// console.log(formatDuration(613662))
-// console.log(formatDuration(61366200))
