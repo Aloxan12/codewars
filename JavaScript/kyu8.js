230b91c309869b99c655de38c0dcf5f736b01471
Aloxan12 <kvakvaaak@mail.ru>
2023-06-06 11:42:45 +0300

    logs

--- JavaScript/kyu8.js
+++ JavaScript/kyu8.js
@@ -195,6 +195,6 @@ solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
     return (((givenMass1 / molarMass1) + (givenMass2 / molarMass2)) * (0.082 * (temp + 273.15))) / volume
 }
 
-function logs(x , a, b){
+function logs(x, a, b) {
     return (Math.log(a) / Math.log(x)) + (Math.log(b) / Math.log(x))
 }
