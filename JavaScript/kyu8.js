ff0b45335b7b2041d2e167ead54493ee77ac6160
Aloxan12 <kvakvaaak@mail.ru>
2023-06-06 11:42:24 +0300

    logs

--- JavaScript/kyu8.js
+++ JavaScript/kyu8.js
@@ -194,3 +194,9 @@ function countBy(x, n) {
 solution = (molarMass1, molarMass2, givenMass1, givenMass2, volume, temp) => {
     return (((givenMass1 / molarMass1) + (givenMass2 / molarMass2)) * (0.082 * (temp + 273.15))) / volume
 }
+
+function logs(x , a, b){
+    return (Math.log(a) / Math.log(x)) + (Math.log(b) / Math.log(x))
+}
+
+console.log('logs', logs(10, 2, 3))
