6315da10f71155e5bec77bba0b855826d84b6d86
Aloxan12 <kvakvaaak@mail.ru>
2023-06-21 12:39:57 +0300

    areaOrPerimeter

--- JavaScript/kyu8.js
+++ JavaScript/kyu8.js
@@ -214,6 +214,5 @@ const areaOrPerimeter = function(l , w) {
     if(l === w)return l * w
     return 2 * (l + w)
 };
-
-console.log(areaOrPerimeter(3, 3)) // 9
-console.log(areaOrPerimeter(6, 10)) // 9
+// console.log(areaOrPerimeter(3, 3)) // 9
+// console.log(areaOrPerimeter(6, 10)) // 32
