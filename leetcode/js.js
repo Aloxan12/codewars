61429373e131c14206e305c42207b84bfaf47e6c
Aloxan12 <kvakvaaak@mail.ru>
2023-06-21 12:27:34 +0300

    leetcode

--- leetcode/js.js
+++ leetcode/js.js
@@ -1,7 +1,10 @@
 const values = {
+    IX: 9,
     CM: 900,
     XC: 90,
+    CD: 400,
     IV: 4,
+    XL: 40,
     'I': 1,
     'V': 5,
     'X': 10,
@@ -16,8 +19,8 @@ var romanToInt = function (s) {
     for (let value in values) {
         result = result.replace(value, ` ${values[value]} `)
     }
-    console.log('result', result)
-    return result.split(' ').map(rome => values[rome]).reduce((acc, el) => acc + +el, 0)
+    const newRes = result.trim().split(' ').map(item => isNaN(+item) ? item.split('').map(i => values[i]).reduce((acc, el)=> acc + el, 0) : item)
+    return newRes.reduce((acc, el) => acc + +el, 0)
 };
 
 console.log(romanToInt('III'))
