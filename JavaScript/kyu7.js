f3e266de7445d20776214117c241df3422c99ae7
Aloxan12 <kvakvaaak@mail.ru>
2023-06-23 12:21:54 +0300

    minMax

--- JavaScript/kyu7.js
+++ JavaScript/kyu7.js
@@ -575,3 +575,7 @@ function removeSmallest(numbers) {
 function helloWorld() {
     return String.fromCharCode(72, 101, 108, 108, 111, 44, 32, 87, 111, 114, 108, 100, 33);
 }
+
+function minMax(arr){
+    return [0,0]; // fix me!
+}
