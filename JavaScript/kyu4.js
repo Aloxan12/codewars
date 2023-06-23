04f1dd445881a0579e3ccaed4cad38828aab1159
Aloxan12 <kvakvaaak@mail.ru>
2023-06-23 12:51:02 +0300

    sameStructureAs

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -100,5 +100,5 @@ Array.prototype.sameStructureAs = function (other) {
 };
 
 // console.log([ 1, 1, 1 ].sameStructureAs([ 2, 2, 2 ]))
-console.log([1,[1,1]].sameStructureAs([[2,2],2]))
-console.log([1,'[',']'].sameStructureAs(['[',']',1]))
+// console.log([1,[1,1]].sameStructureAs([[2,2],2]))
+// console.log([1,'[',']'].sameStructureAs(['[',']',1]))
