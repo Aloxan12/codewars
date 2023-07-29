1cffb40f748a4c89b7433539d4f56cf884316db2
Aloxan12 <kvakvaaak@mail.ru>
2023-07-29 18:19:50 +0300

    solution_4

--- TypeScript/kyu6.ts
+++ TypeScript/kyu6.ts
@@ -357,3 +357,8 @@ function isPrime(num: number) {
 function expandedForm(num: number) {
     return `${num}`.split('').reverse().map((number, index)=> Number(number) * Math.pow(10, index)).reverse().filter(item => !!item).join(' + ')
 }
+
+// Break camelCase
+function solution_4(string: string) {
+    return string.replace(/[A-Z]/g, (letter)=> ` ${letter}`)
+}
