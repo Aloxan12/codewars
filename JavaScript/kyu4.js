cade52ff98519e297582cc76cee0ddb776f267d9
Aloxan12 <kvakvaaak@mail.ru>
2023-11-01 10:30:13 +0300

    getFileName

--- JavaScript/kyu4.js
+++ JavaScript/kyu4.js
@@ -102,13 +102,3 @@ Array.prototype.sameStructureAs = function (other) {
 // console.log([ 1, 1, 1 ].sameStructureAs([ 2, 2, 2 ]))
 // console.log([1,[1,1]].sameStructureAs([[2,2],2]))
 // console.log([1,'[',']'].sameStructureAs(['[',']',1]))
-
-
-var o = {
-    prop: 1,
-    fn: function (){
-        console.log('fn', this)
-    }
-}
-
-o.fn()
